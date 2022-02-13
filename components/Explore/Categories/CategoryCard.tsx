57d8f7222f7aa541ede69b081c55fb44f1dd06dd
KFuee <jbisazac@icloud.com>
2022-02-13 16:18:44 +0100

    Retocadas tarjetas de categoría en explorar

--- components/Explore/Categories/CategoryCard.tsx
+++ components/Explore/Categories/CategoryCard.tsx
@@ -38,7 +38,7 @@ const styles = StyleSheet.create({
     height: "100%",
     justifyContent: "center",
     alignItems: "center",
-    backgroundColor: "rgba(255, 71, 96, 0.5)",
+    backgroundColor: "rgba(150, 71, 96, 0.5)",
   },
 
   titleText: {

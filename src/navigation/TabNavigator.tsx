72c0299a26ddd1029181a365e73b1af55151e04d
KFuee <jbisazac@icloud.com>
2022-03-14 16:40:46 +0100

    Nuevo diseño del bottom tab navigator

--- src/navigation/TabNavigator.tsx
+++ src/navigation/TabNavigator.tsx
@@ -1,33 +1,52 @@
-import {
-  BottomTabBarProps,
-  createBottomTabNavigator,
-} from '@react-navigation/bottom-tabs';
-
-import TabBar from '../components/TabBar';
+import React from 'react';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { FontAwesome5 } from '@expo/vector-icons';
 
 import { StackHome, StackExplore } from './StackNavigator';
 
-const BottomTabNavigator = createBottomTabNavigator();
+const Tab = createBottomTabNavigator();
 
 function TabNavigator() {
-  const renderTabBar = (props: BottomTabBarProps) => (
-    <TabBar
-      descriptors={props.descriptors}
-      state={props.state}
-      navigation={props.navigation}
-    />
+  const renderTabBarIcon = (
+    name: string | undefined,
+    size: number,
+    focused: boolean,
+    color: string,
+  ) => (
+    <FontAwesome5 name={name} size={size} color={focused ? '#FF4760' : color} />
   );
 
   return (
-    <BottomTabNavigator.Navigator
+    <Tab.Navigator
       initialRouteName="HomeTab"
-      tabBar={props => renderTabBar(props)}
-      screenOptions={{ headerShown: false }}
-    >
-      <BottomTabNavigator.Screen name="HomeTab" component={StackHome} />
+      screenOptions={({ route }) => ({
+        headerShown: false,
+        tabBarActiveTintColor: '#FF4760',
+        tabBarIcon: ({ size, focused, color }) => {
+          let iconName;
 
-      <BottomTabNavigator.Screen name="ExploreTab" component={StackExplore} />
-    </BottomTabNavigator.Navigator>
+          if (route.name === 'HomeTab') {
+            iconName = 'home';
+          } else if (route.name === 'ExploreTab') {
+            iconName = 'search';
+          }
+
+          return renderTabBarIcon(iconName, size, focused, color);
+        },
+      })}
+    >
+      <Tab.Screen
+        name="HomeTab"
+        component={StackHome}
+        options={{ title: 'Home' }}
+      />
+
+      <Tab.Screen
+        name="ExploreTab"
+        component={StackExplore}
+        options={{ title: 'Explorar' }}
+      />
+    </Tab.Navigator>
   );
 }
 

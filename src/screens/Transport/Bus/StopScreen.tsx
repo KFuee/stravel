c0e8a8f7602fa09c65769736eebd1590f599d871
KFuee <jbisazac@icloud.com>
2022-03-28 22:11:37 +0200

    Pull to refresh en scrollview de parada

--- src/screens/Transport/Bus/StopScreen.tsx
+++ src/screens/Transport/Bus/StopScreen.tsx
@@ -1,17 +1,34 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { useState, useCallback } from 'react';
+import { StyleSheet, Text, ScrollView, RefreshControl } from 'react-native';
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: '#FFF',
   },
 });
 
 export default function BusStopScreen({ route }: any) {
   const { stop } = route.params;
 
+  const [refreshing, setRefreshing] = useState(false);
+
+  const onRefresh = useCallback(() => {
+    setRefreshing(true);
+
+    setTimeout(() => {
+      setRefreshing(false);
+    }, 1000);
+  }, []);
+
   return (
-    <View style={styles.container}>
+    <ScrollView
+      style={styles.container}
+      refreshControl={
+        <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+      }
+    >
       <Text>{stop.name}</Text>
-    </View>
+    </ScrollView>
   );
 }

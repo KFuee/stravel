f5b4c1b94cb47b110944538734bc442418fc374e
KFuee <jbisazac@icloud.com>
2022-05-11 18:49:15 +0200

    Comienzo de nueva vista home, welcome con username

---
+++ src/components/Home/Welcome.tsx
@@ -0,0 +1,83 @@
+import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
+import { FontAwesome5 } from '@expo/vector-icons';
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    paddingHorizontal: 16,
+  },
+
+  rowContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+
+  avatarContainer: {
+    width: 65,
+    height: 65,
+    marginRight: 16,
+    borderRadius: 50,
+    backgroundColor: '#FF4760',
+    justifyContent: 'center',
+    alignItems: 'center',
+    overflow: 'hidden',
+  },
+
+  avatarImage: {
+    width: 50,
+    height: 50,
+    bottom: -7,
+  },
+
+  usernameText: {
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+
+  qrIconContainer: {
+    width: 40,
+    height: 40,
+    borderRadius: 50,
+    backgroundColor: '#F5F5F5',
+    alignItems: 'center',
+    justifyContent: 'center',
+    overflow: 'hidden',
+  },
+});
+
+function Welcome({ username }: { username: string }) {
+  return (
+    <View style={styles.container}>
+      <View style={styles.rowContainer}>
+        <View style={styles.avatarContainer}>
+          <Image
+            source={require('../../assets/images/home/user.png')}
+            style={styles.avatarImage}
+          />
+        </View>
+
+        <View>
+          <View style={styles.rowContainer}>
+            <Text style={{ marginBottom: 4 }}>Bienvenido 👋</Text>
+          </View>
+
+          <Text style={styles.usernameText}>{username}</Text>
+        </View>
+      </View>
+
+      <TouchableOpacity
+        style={styles.qrIconContainer}
+        onPress={() => {}}
+        activeOpacity={0.8}
+      >
+        <View>
+          <FontAwesome5 name="qrcode" size={24} color="#FF4760" />
+        </View>
+      </TouchableOpacity>
+    </View>
+  );
+}
+
+export default Welcome;
--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,8 +1,10 @@
-import { useRef } from 'react';
-import { Animated, View, StyleSheet } from 'react-native';
+import { StyleSheet, SafeAreaView } from 'react-native';
 
-import WelcomeBanner from '../components/Home/WelcomeBanner';
-import Suggestions from '../components/Home/Suggestions';
+// contexts
+import { useAuth } from '../contexts/AuthContext';
+
+// components
+import Welcome from '../components/Home/Welcome';
 
 const styles = StyleSheet.create({
   container: {
@@ -12,26 +14,12 @@ const styles = StyleSheet.create({
 });
 
 function HomeScreen() {
-  const scroll = useRef(new Animated.Value(0)).current;
-
-  const onScroll = Animated.event(
-    [{ nativeEvent: { contentOffset: { y: scroll } } }],
-    { useNativeDriver: false },
-  );
+  const { authData } = useAuth();
 
   return (
-    <View style={styles.container}>
-      <WelcomeBanner scroll={scroll} />
-
-      <Animated.ScrollView
-        onScroll={onScroll}
-        bounces={false}
-        scrollEventThrottle={16}
-        showsVerticalScrollIndicator={false}
-      >
-        <Suggestions />
-      </Animated.ScrollView>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Welcome username={authData!.user.name} />
+    </SafeAreaView>
   );
 }
 
